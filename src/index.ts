85df876eb2baa79bf9fb37e40d84e083ef7d3d73
altox <xeroxaltox@gmail.com>
2018-02-04 02:42:39 +0300

    fix exports

--- src/index.ts
+++ src/index.ts
@@ -3,79 +3,78 @@
  */
 
 export * from './modules/serialize/BaseSerializer'
-export * from './modules/serialize/JSONSerializer'
+export {default as JSONSerializer} from './modules/serialize/JSONSerializer'
 
 /**
  * logger
  */
 
 export * from './modules/logger/domain/BaseLogger'
-export * from './modules/logger/infrustructure/ConsoleLogger'
+export {default as ConsoleLogger} from './modules/logger/infrustructure/ConsoleLogger'
 
 
 /**
  * apiServer
  */
 
-export * from './modules/apiServer/application/AirshipAPIServer'
-export * from './modules/apiServer/domain/entity/ASErrorResponse'
+export {default as AirshipAPIServer} from './modules/apiServer/application/AirshipAPIServer'
+export {default as ASErrorResponse} from './modules/apiServer/domain/entity/ASErrorResponse'
 export * from './modules/apiServer/domain/entity/ASRequest'
 export * from './modules/apiServer/domain/entity/ASResponse'
-export * from './modules/apiServer/domain/entity/ASSuccessResponse'
-export * from './modules/apiServer/domain/entity/ASSuccessResponse'
+export {default as ASSuccessResponse} from './modules/apiServer/domain/entity/ASSuccessResponse'
 export * from './modules/apiServer/domain/BaseRequestHandler'
 export * from './modules/apiServer/domain/RequestsProvider'
 export * from './modules/apiServer/domain/ServerConfig'
-export * from './modules/apiServer/infrastructure/HttpRequestsProvider'
+export {default as HttpRequestsProvider} from './modules/apiServer/infrastructure/HttpRequestsProvider'
 export * from './modules/apiServer/infrastructure/MultyRequestHandler'
-export * from './modules/apiServer/infrastructure/RequestHandlersManager'
+export {default as RequestHandlersManager} from './modules/apiServer/infrastructure/RequestHandlersManager'
 
 /**
  * cache
  */
 
 export * from './modules/cache/domain/BaseCache'
-export * from './modules/cache/infrustructure/MemoryCache'
+export {default as MemoryCache} from './modules/cache/infrustructure/MemoryCache'
 
 /**
  * statistics
  */
 
  export * from './modules/statistics/domain/BaseStatisticsCounter'
- export * from './modules/statistics/infrastructure/LocalStatisticsCounter'
+ export {default as LocalStatisticsCounter} from './modules/statistics/infrastructure/LocalStatisticsCounter'
 
  /**
   * codeGen
   */
 
-export * from './modules/codeGen/domain/schema/ApiMethodParam'
-export * from './modules/codeGen/domain/schema/ApiMethodScheme'
-export * from './modules/codeGen/domain/schema/ClassField'
-export * from './modules/codeGen/domain/schema/ClassScheme'
-export * from './modules/codeGen/domain/types/AnyType'
-export * from './modules/codeGen/domain/types/BooleanType'
-export * from './modules/codeGen/domain/types/CustomType'
-export * from './modules/codeGen/domain/types/IntBoolType'
-export * from './modules/codeGen/domain/types/NumberType'
-export * from './modules/codeGen/domain/types/ObjectType'
-export * from './modules/codeGen/domain/types/StringType'
+export {default as ApiMethodParam} from './modules/codeGen/domain/schema/ApiMethodParam'
+export {default as ApiMethodScheme} from './modules/codeGen/domain/schema/ApiMethodScheme'
+export {default as ClassField} from './modules/codeGen/domain/schema/ClassField'
+export {default as ClassScheme} from './modules/codeGen/domain/schema/ClassScheme'
+export {default as AnyType} from './modules/codeGen/domain/types/AnyType'
+export {default as BooleanType} from './modules/codeGen/domain/types/BooleanType'
+export {default as CustomType} from './modules/codeGen/domain/types/CustomType'
+export {default as IntBoolType} from './modules/codeGen/domain/types/IntBoolType'
+export {default as NumberType} from './modules/codeGen/domain/types/NumberType'
+export {default as ObjectType} from './modules/codeGen/domain/types/ObjectType'
+export {default as StringType} from './modules/codeGen/domain/types/StringType'
 export * from './modules/codeGen/domain/types/Type'
-export * from './modules/codeGen/domain/types/VectorType'
+export {default as VectorType} from './modules/codeGen/domain/types/VectorType'
 export * from './modules/codeGen/domain/CodeGenerator'
-export * from './modules/codeGen/domain/CodeLine'
-export * from './modules/codeGen/domain/SourceCode'
-export * from './modules/codeGen/infrastructure/JavaScriptCodeGenerator'
-export * from './modules/codeGen/infrastructure/SwiftCodeGenerator'
-export * from './modules/codeGen/infrastructure/TypescriptCodeGenerator'
+export {default as CodeLine} from './modules/codeGen/domain/CodeLine'
+export {default as SourceCode} from './modules/codeGen/domain/SourceCode'
+export {default as JavaScriptCodeGenerator} from './modules/codeGen/infrastructure/JavaScriptCodeGenerator'
+export {default as SwiftCodeGenerator} from './modules/codeGen/infrastructure/SwiftCodeGenerator'
+export {default as TypescriptCodeGenerator} from './modules/codeGen/infrastructure/TypescriptCodeGenerator'
 export * from './modules/codeGen/infrastructure/Utils'
 
 /**
  * schemeGenerator
  */
 
-export * from './modules/schemeGenerator/domain/ApiSchema'
+export {default as ApiSchema} from './modules/schemeGenerator/domain/ApiSchema'
 export * from './modules/schemeGenerator/domain/ApiSchemeGenerator'
-export * from './modules/schemeGenerator/infrastructure/AirshipApiSchemeGenerator'
+export {default as AirshipApiSchemeGenerator} from './modules/schemeGenerator/infrastructure/AirshipApiSchemeGenerator'
 
 /**
  * sdkGenerator
@@ -83,5 +82,5 @@ export * from './modules/schemeGenerator/infrastructure/AirshipApiSchemeGenerato
 
 export * from './modules/sdkGenerator/domain/ApiSDKGenerator'
 export * from './modules/sdkGenerator/domain/SDKConfig'
-export * from './modules/sdkGenerator/domain/SDKFile'
-export * from './modules/sdkGenerator/infrastructure/AirshipApiSDKGenerator'
+export {default as SDKFile} from './modules/sdkGenerator/domain/SDKFile'
+export {default as AirshipApiSDKGenerator} from './modules/sdkGenerator/infrastructure/AirshipApiSDKGenerator'
